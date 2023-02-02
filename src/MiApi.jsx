58c219ccc6817b0edd87058ed53754ecc8def23b
Osvaldo Martínez S <osmarse@gmail.com>
2023-02-02 20:52:30 -0300

    Exame React 1

---
+++ src/MiApi.jsx
@@ -0,0 +1,95 @@
+import React, { useState, useEffect } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Navbar from 'react-bootstrap/Navbar';
+import logo from "./logo.png";
+import "./index.css"
+
+const MiApi = () => {
+
+    const [datos, setDatos] = useState([])
+    const [busqueda, setBusqueda] = useState("")
+    const [orden, setOrden] = useState(true)
+
+    const entradaDatos = (e) => {
+        setBusqueda(e.target.value)
+    }
+
+    const clickOrden = () => {
+        setOrden(!orden)
+    }
+
+    const filtroBusqueda = datos.filter(item => item.nombre.toLowerCase().includes(busqueda.toLowerCase())).sort((a, b) => {
+        if (a.nombre < b.nombre) return orden ? -1 : 1
+        if (a.nombre > b.nombre) return orden ? 1 : -1
+        return 0
+    })
+
+    const consumoApi = async () => {
+        const url = ""
+        try {
+            let respuesta = await fetch(url)
+            let nuevaData = await respuesta.json()
+            setDatos(nuevaData)
+        } catch {
+            alert("Error de consumo de API")
+        }
+    }
+
+    useEffect(() => {
+        consumoApi()
+    }, [])
+
+
+    return (
+        <>
+            <div>
+                <Navbar bg="secondary" variant="secondary">
+                    <Container>
+                        <Navbar.Brand href="#home">
+                            <img
+                                src={logo}
+
+                                className=" logo d-inline-block align-top"
+                            />
+                        </Navbar.Brand>
+                    </Container>
+                </Navbar>
+                <Container>
+                    <Row>
+                        <Col>
+                            <div className="w-100 mt-3 d-flex">
+                                <Button className="me-1" variant="secondary" onClick={clickOrden}>Ordenar por Nombres </Button>
+                                <input className="w-50" type="text" value={busqueda} onChange={entradaDatos} placeholder="Buscar personaje" />
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+                <Container className="my-3">
+                    <Row xs={1} md={3} className="g-4">
+                        {filtroBusqueda.map(item => (
+                            <Col>
+                                <Card class="m-5 p-2 bg-light border" style={{ width: '20rem' }}>
+                                    <Card.Img className="fotos mt-3 border" src={item.foto} variant="top" />
+                                    <Card.Body>
+                                        <Card.Title>{item.nombre}</Card.Title>
+                                        <Card.Text>Doblajista: {item.doblaje}</Card.Text>
+                                        <Button variant="secondary">Info</Button>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                        ))}
+                    </Row>
+                </Container>
+
+            </div>
+        </>
+
+    )
+}
+
+export default MiApi
---
+++ src/lista.js
@@ -0,0 +1,40 @@
+const lista = [
+    {
+        "id": 1,
+        "nombre": "Rick Hunter",
+        "doblaje": "Jesus Barrero",
+        "foto": "https://i.ytimg.com/vi/8O43AuFNehE/hqdefault.jpg"
+    },
+    {
+        "id": 2,
+        "nombre": "Lisa Hayes",
+        "doblaje": "Patricia Acevedo",
+        "foto": "https://static.wikia.nocookie.net/robotech/images/5/57/Lisahayes.jpg"
+    },
+    {
+        "id": 3,
+        "nombre": "Lynn Minmei",
+        "doblaje": "Sylvia Garcel",
+        "foto": "https://pm1.narvii.com/6753/e69fe6b74037ebfc75c2c4c582831f4ac90e76cav2_hq.jpg"
+    },
+    {
+        "id": 4,
+        "nombre": "Max Sterling",
+        "doblaje": "Raul Aldana",
+        "foto": "https://comicvine.gamespot.com/a/uploads/original/0/5344/1632187-max_sterling_02.png"
+    },
+    {
+        "id": 5,
+        "nombre": "Roy Fokker",
+        "doblaje": "Moisés Palacios",
+        "foto": "https://ghostlightning.files.wordpress.com/2011/07/sdf-macross-episode-34-remastered-galaxy-network-mkv_snapshot_01-32_2011-07-10_16-29-15.jpg"
+    },
+    {
+        "id": 6,
+        "nombre": "Miriya Parina",
+        "doblaje": "Diana Perez",
+        "foto": "https://64.media.tumblr.com/ee2f9c09d9c72d484442498cf502cb67/tumblr_ph0ietLWOY1wgv564o8_1280.png"
+    }
+]
+
+export default lista
